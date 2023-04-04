d76f75ace45de253621199186903a5deb4d95b32
saicharanpogul <pogul1804@gmail.com>
2023-04-04 15:57:17 +0530

    Bug fixes(3)

--- app/src/assets/SVGs/empty.tsx
+++ app/src/assets/SVGs/empty.tsx
@@ -15,9 +15,9 @@ const empty = (
       <path
         d="M50.0001 33.3333V50M50.0001 66.7083L50.0417 66.6625M37.5001 12.5H16.6667V25M16.6667 45.8333V54.1667M83.3334 45.8333V54.1667M62.5001 12.5H83.3334V25M37.5001 87.5H16.6667V75M62.5001 87.5H83.3334V75"
         stroke="#4A5568"
-        stroke-width="6.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="6.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
